3b629f63c005205de983c442c595d71250085a5e
Mukul <budhirajasmukul8@gmail.com>
2026-01-30 22:02:43 +0530

    fix auth middleware (remove next headers)

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt=require("jsonwebtoken");
 const User=require("../models/User");
-const { headers } = require("next/headers");
+
 const protect=async(req,res,next)=>{
     try{
         let token;
